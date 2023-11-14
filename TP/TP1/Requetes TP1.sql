0a4e918907401d1f2965f9b29468aca81788b2ba
antoninpomies@gmail.com <antoninpomies@gmail.com>
2023-11-14 11:10:05 +0100

    Update requests

--- TP/TP1/Requetes TP1.sql
+++ TP/TP1/Requetes TP1.sql
@@ -19,6 +19,12 @@ select s.libserv, count(e.numemp) from service s left join employe e on e.numser
 --Question n°10
 select s.libserv, count(*) from service s inner joint employe e on s.numserv = e.numserv group by s.libserv having count(*) = (select max(count(*)) from employe e1 group by e1.numserv)
 --Question n°11
-
+select s.libserv from service s inner join employe e on s.numserv = e.numserv group by s.libserv having count(*) > (select avg(count(e1.numserv)) from service s1 inner join employe e1 on s1.numserv = e1.numserv group by s1.numserv);
 --Question n°12
-select s.libserv, e.hom, e.prenom, f.salaire from employe einner join service son  e.numserv = s.numsev inner join fonction f on e.numfonct = f.numfonct and f.salaire = (select max (f1.salaire) from  employe e1 inner join fonction f1 on e1.numfonct = f1.numfonct where e1.numserv = s.numserv);
+select s.libserv, e.nom, e.prenom, f.salaire from employe e inner join service s on e.numserv = s.numserv inner join fonction f on e.numfonct = f.numfonct and f.salaire = (select max (f1.salaire) from  employe e1 inner join fonction f1 on e1.numfonct = f1.numfonct where e1.numserv = s.numserv);
+--Question n°13
+select s.libserv, f.libfonct, count(*) from employe e inner join service s on e.numserv = s.numserv inner jion fonction f on e .numfonct = f.numfonct group by s.libserv, f.livfonct, e.numserv having count(*) = (select max(count(*)) from employe e1 where e1.numserv = e.numserv group by e1.numfonct) order by s.libserv;
+--Question n°14
+select f.libfonct, f.libserv, count(*) from employe e inner join service s on e.numserv = s.numserv inner jion fonction f on e .numfonct = f.numfonct group by s.libserv, f.livfonct, e.numfonct having count(*) = (select max(count(*)) from employe e1 where e1.numfonct = e.numfonct group by e1.numserv) order by s.libfonct;
+--Question n°15
+select libserv, sum(salaire) from employe e inner join service s on e.numserv = s.numserv inner join fonction f on e.numfonct = f.numfonct group by libserv having sum(salaire) = (select max(sum(salaire)) from employe e1 inner join fonction f1 on e1.numfonct = f1.numfonct group by e1.numserv)
