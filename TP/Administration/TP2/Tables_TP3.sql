519582a49be30e097d93a34a422a9bdb6b48944b
antoninpomies@gmail.com <antoninpomies@gmail.com>
2023-12-13 08:50:11 +0100

    Mise a jour Tables.sql

--- TP/Administration/TP2/Tables_TP3.sql
+++ TP/Administration/TP2/Tables_TP3.sql
@@ -1,26 +1,45 @@
-CREATE TABLE CLIENTS (
-    idCli INT PRIMARY KEY,
-    nom NUMBER(5) NOT NULL CHECK (nom = UPPER(nom)),
-    prenom VARCHAR2(20) NOT NULL CHECK (prenom = INITCAP(prenom)),
-    nbLoc NUMBER(3),
-    FOREIGN KEY (idCli) REFERENCES LOCATIONS (idCli)
-);
+create table client (
+IdCli NUMBER(5) constraint client_pk primary key,
+nom VARCHAR2(20) not null constraint nom_cli_maj check (nom = UPPER(nom)),
+prenom VARCHAR2(20) not null,
+nbloc NUMBER(3) default 0);
 
-CREATE TABLE VEHICULES (
-    idVehi NUMBER(6),
-    typeV VARCHAR2(7),
-    libelle VARCHAR2(60),
-    PRIMARY KEY (idVehi, typeV),
-    FOREIGN KEY (idVehi) REFERENCES LOCATIONS (idVehi),
-    FOREIGN KEY (typeV) REFERENCES LOCATIONS (typeV)
-);
+create table prixloc (
+TypeV VARCHAR2(7) constraint prixloc_pk primary key,
+prix NUMBER(6,2) constraint prix_positif check (prix > 0),
+constraint typeV_val check (TypeV in ('Petit', 'Moyen', 'Grand')));
 
-CREATE TABLE LOCATIONS (
-    idCli NUMBER(5),
-    idVehi NUMBER(6),
-    typeV VARCHAR(2),
-    dateDeb DATE,
-    dateFin DATE,
-    FOREIGN KEY (typeV) REFERENCES PRIXLOC (typeV)
-    
-)
+create table vehicules (
+IdVehi NUMBER(6) ,
+TypeV VARCHAR2(7) constraint vehi_ref_prix references prixloc (TypeV),
+libelle VARCHAR2(60),
+constraint vehicules_pk primary key (IdVehi, TypeV));
+
+create table location (
+IdCli NUMBER(5) constraint loc_ref_cli references client(IdCli),
+IdVehi NUMBER(6),
+TypeV VARCHAR2(7),
+DateDeb date,
+dateFin date,
+constraint location_pk primary key (IdVehi, TypeV, DateDeb),
+constraint loc_ref_vehi foreign key (IdVehi, TypeV) references vehicules (IdVehi, TypeV),
+constraint date_loc_coherent check (dateFin >= DateDeb));
+
+
+create table factures (
+IdCli NUMBER(5) constraint factures_ref_client references client (IdCli),
+DateFact date,
+montantF NUMBER(6,2),
+constraint factures_pk primary key (IdCli, DateFact));
+
+
+create table penalites (
+IdCli NUMBER(5) constraint penaltes_ref_client references client (IdCli),
+DatePen date,
+montantP NUMBER(4,2),
+constraint penalites_pk primary key (IdCli, DatePen));
+
+
+create table retard (
+NbJour NUMBER(2) constraint retard_pk primary key,
+mtP NUMBER(4,2));
